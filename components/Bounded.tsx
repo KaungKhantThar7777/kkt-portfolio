9637264d54f4f39085aa9355fad4bc4ef6f2e3a7
kaungkhantthar7777 <kaungkhantthar77@gmail.com>
2024-01-14 16:52:54 +0630

    added: Bounded component

---
+++ components/Bounded.tsx
@@ -0,0 +1,27 @@
+import clsx from "clsx"
+import React from "react"
+
+type BoundedProps = {
+    as?: React.ElementType,
+    children: React.ReactNode,
+    className?: string
+}
+
+const Bounded = React.forwardRef<HTMLDivElement, BoundedProps>((
+    { as: Comp = "section",
+        className,
+        children,
+        ...restProps }, ref) => {
+
+    return (
+        <Comp ref={ref} className={clsx("px-4 py-10 md:px-6 md:py-14 lg:py-16")} {...restProps}>
+            <div className="mx-auto w-full max-w-7xl">{children}</div>
+        </Comp>
+    )
+})
+
+
+Bounded.displayName = "Bounded"
+
+
+export default Bounded
--- slices/Hero/index.tsx
+++ slices/Hero/index.tsx
@@ -1,4 +1,5 @@
 "use client"
+import Bounded from "@/components/Bounded";
 import { Content, KeyTextField } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
 import gsap from "gsap";
@@ -61,7 +62,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
     ))
   }
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
       ref={component}
@@ -77,7 +78,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
         </div>
         <div></div>
       </div>
-    </section>
+    </Bounded>
   );
 };
 
